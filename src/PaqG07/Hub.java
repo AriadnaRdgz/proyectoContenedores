dcc4c1dd23ed98ac8a257c86b1a61f7b8c14b097
Racope <racope9@gmail.com>
2023-03-20 19:54:28 +0100

    Creo que ya se qué hacer

--- src/PaqG07/Hub.java
+++ src/PaqG07/Hub.java
@@ -20,4 +20,31 @@ public class Hub {
 
     }
 
+    public void AñadirContenedor(Contenedor contenedorañadido){
+        Contenedor contenedoraux = contenedorañadido;
+        for (int i=0; i<=this.hub.length; i++){
+            for (int j=0; j<=this.hub[0].length;j++){
+                Contenedor contenedor = hub[i][j];
+                if (hub[i][j]==null){
+                    hub[i][j]=contenedoraux;
+                    break;
+                }
+                if(hub[i][j].getPrioridad() >= contenedoraux.getPrioridad()){
+                    hub[i][j]=contenedoraux;
+                    contenedoraux = contenedor;
+                }
+            }
+        }
+    }
+
+    public void QuitarContenedor(int columna){
+
+        for (int i=0; i<=this.hub.length; i++){
+            if(hub[i][columna]!=null){
+                hub[i][columna]=null;
+                break;
+            }
+        }
+    }
+
 }
