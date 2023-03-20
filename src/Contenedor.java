6cf359aa2e65fa69f94c4d77f0874736e7c213f4
Ari <ariadnarofdez@gmail.com>
2023-03-20 19:06:57 +0100

    atributos

--- src/Contenedor.java
+++ src/Contenedor.java
@@ -1,3 +1,13 @@
 public class Contenedor {
 
+        private int id;
+        private int peso;
+        private String país;
+        private boolean inspeccionado;
+        private int prioridad;
+        private String descripción;
+        private String empresaEmisora;
+        private String empresaReceptora;
+
+
 }
