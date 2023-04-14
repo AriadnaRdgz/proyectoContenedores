5e21a271fe70c918a651d16ce6700a96798d4963
Ari <ariadnarofdez@gmail.com>
2023-04-14 12:23:12 +0200

    Oyente

--- src/PaqG07/Main.java
+++ src/PaqG07/Main.java
@@ -1,10 +1,14 @@
 package PaqG07;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         PaquitaSalas myFrame = new PaquitaSalas();
 
-
+        JButton boton = new JButton();
+        boton.addActionListener(new Oyente());
+        
 
     }
 }
---
+++ src/PaqG07/Oyente.java
@@ -0,0 +1,13 @@
+package PaqG07;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Oyente implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
